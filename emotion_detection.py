c272e8346db57c4af7455fed3720fc6cb24f834e
Anantha Narayanan Varadharajan <anantha.varadharajan@hpe.com>
2024-11-21 15:28:16 +0100

    emotion_detetcion

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,17 +1,15 @@
 import requests
 
-# Define the emotion detection function
-def emotion_detector(text_to_analyze):
-    # Define the endpoint and headers
+import json
+
+def emotion_detector(text_to_analyse):
+
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    
-    # Construct the input JSON
-    input_json = {"raw_document": {"text": text_to_analyze}}
-    
-    # Send POST request to the Emotion Predict API
-    response = requests.post(url, headers=headers, json=input_json)
-    
-    # Extract the text attribute from the response and return it
-    return response.json().get('text', {})
-
+    myobj = {"raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, json = myobj, headers = headers)
+    res = json.loads(response.text)
+    formatted_output = res["emotionPredictions"][0]["emotion"]
+    dominant_emotion = max(formatted_output, key = lambda x: formatted_output[x])
+    formatted_output["dominant_dictionary"] = dominant_emotion
+    return formatted_output
